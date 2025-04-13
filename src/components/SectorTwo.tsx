18b52f3a3d85e125b158bbd75559c034730a5900
adrianoandriole <adrianoandriole@gmail.com>
2025-04-13 06:30:30 -0300

    Quarto commit

--- src/components/SectorTwo.tsx
+++ src/components/SectorTwo.tsx
@@ -1,10 +1,8 @@
 import {Component} from "solid-js";
 import styles from "./SectorTwo.module.css";
-import instagram_icon from "../assets/instagram_icon.png";
 import showcase_1 from "../assets/showcase_1.png";
 import showcase_3 from "../assets/showcase_3.png";
 import showcase_2 from "../assets/showcase_2.png";
-import showcase_4 from "../assets/showcase_4.png";
 
 const SectorTwo: Component = () => {
     return (
@@ -12,9 +10,9 @@ const SectorTwo: Component = () => {
 
             <div class={styles.container}>
 
-                <img class={styles.card1} src={showcase_1}> </img>
-                <img class={styles.card1} src={showcase_3}> </img>
-                <img class={styles.card1} src={showcase_2}> </img>
+                <img class={styles.card1} src={showcase_1} alt="Imagem do projeto 1"/>
+                <img class={styles.card1} src={showcase_3} alt="Imagem do projeto 3"/>
+                <img class={styles.card1} src={showcase_2} alt="Imagem do projeto 2"/>
 
             </div>
 
